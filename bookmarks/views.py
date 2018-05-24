7a4b33f11994be9445a0b42115b1e0abb3060be1
sampath <sampath.surineni@oracle.com>
2018-05-24 09:03:12 +0530

    returning html string

--- bookmarks/views.py
+++ bookmarks/views.py
@@ -3,4 +3,16 @@ from django.shortcuts import render
 
 
 def main_page(request):
-    return HttpResponse('Welcome to Django bookmarks')
+    output = '''
+    <html>
+    <head><title>%s</title></head>
+    <body>
+    <h1>%s</h1>
+    <p>%s</p>
+    </body>
+    </html>''' % (
+        'Django Bookmarks',
+        'Welcome to Django Bookmarks',
+        'Where you can store and share bookmarks!')
+
+    return HttpResponse(output)
