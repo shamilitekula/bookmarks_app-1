fd2a059dd5d3dd15ea6f61eb644eaa2889e71c91
sampath <sampath.surineni@oracle.com>
2018-05-25 09:15:36 +0530

    added link model to database

--- bookmarks_app/settings.py
+++ bookmarks_app/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'bookmarks.apps.BookmarksConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
